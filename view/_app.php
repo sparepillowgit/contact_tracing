547f56b904b1ab7e5eed1c914273889305b669fd
sparepillowgit <heropedmeintothis@gmail.com>
2021-08-02 02:31:03 +1000

    Style changes

--- view/_app.php
+++ view/_app.php
@@ -11,7 +11,7 @@ class App {
                 echo "<h1><i class=\"fas fa-search-location icon\"></i> COVID <span class=\"text-contact\">Contact</span> <span class=\"text-tracing\">Tracing</span></h1>";
                 echo "</div>";
             echo "</div>";
-            echo "<div class=\"row\">";
+            echo "<div class=\"row map-row g-0\">";
                 echo "<div class=\"col-12\" id=\"map\">";
                 echo "</div>";
             echo "</div>";
@@ -23,19 +23,19 @@ class App {
         echo "></script>\"";
 
         echo "<div class=\"container-fluid\" id=\"contact-popup\">";
-        echo "<div class=\"row d-flex justify-content-center align-items-center\">";
-            echo "<div class=\"col-11 col-md-3 box\">";
-                echo "<h2 id=\"place\"></h2>";
-                echo "<p id=\"risk\"></p>";
-                echo "<div class=\"separator\"></div>";
-                echo "<p id=\"address\"></p>";
-                echo "<p id=\"suburb\"></p>";
-                echo "<p id=\"date\"></p>";
-                echo "<p id=\"arrival-time\"></p>";
-                echo "<p id=\"departure-time\"></p>";
-                echo "<p id=\"close-popup\"><i class=\"fas fa-times\"></i></p>";
+            echo "<div class=\"row d-flex justify-content-center align-items-center\">";
+                echo "<div class=\"col-11 col-md-3 box\">";
+                    echo "<h2 id=\"place\"></h2>";
+                    echo "<p id=\"risk\"></p>";
+                    echo "<div class=\"separator\"></div>";
+                    echo "<p id=\"address\"></p>";
+                    echo "<p id=\"suburb\"></p>";
+                    echo "<p id=\"date\"></p>";
+                    echo "<p id=\"arrival-time\"></p>";
+                    echo "<p id=\"departure-time\"></p>";
+                    echo "<p id=\"close-popup\"><i class=\"fas fa-times\"></i></p>";
+                echo "</div>";
             echo "</div>";
-        echo "</div>";
         echo "</div>";
     }
 
