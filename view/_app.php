2a714cd95fb61935379cf57ade3fdb38490b2fc5
sparepillowgit <heropedmeintothis@gmail.com>
2021-08-02 11:13:21 +1000

    Quick clean

--- view/_app.php
+++ view/_app.php
@@ -5,38 +5,38 @@ class App {
     function init() {
         $args = func_get_args();
 
-        echo "<div class=\"container-fluid main\">";
-            echo "<div class=\"row header justify-content-center justify-content-md-start\">";
-                echo "<div class=\"col-8 col-md-3 text-center\">";
-                echo "<h1><i class=\"fas fa-search-location icon\"></i> COVID <span class=\"text-contact\">Contact</span> <span class=\"text-tracing\">Tracing</span></h1>";
-                echo "</div>";
-            echo "</div>";
-            echo "<div class=\"row map-row g-0\">";
-                echo "<div class=\"col-12\" id=\"map\">";
-                echo "</div>";
-            echo "</div>";
-        echo "</div>";
+        echo "<div class=\"container-fluid main\">\n";
+            echo "<div class=\"row header justify-content-center justify-content-md-start\">\n";
+                echo "<div class=\"col-8 col-md-3 text-center\">\n";
+                echo "<h1><i class=\"fas fa-search-location icon\"></i> COVID <span class=\"text-contact\">Contact</span> <span class=\"text-tracing\">Tracing</span></h1>\n";
+                echo "</div>\n";
+            echo "</div>\n";
+            echo "<div class=\"row map-row g-0\">\n";
+                echo "<div class=\"col-12\" id=\"map\">\n";
+                echo "</div>\n";
+            echo "</div>\n";
+        echo "</div>\n";
 
         echo "<script ";
         echo "src=\"https://maps.googleapis.com/maps/api/js?key=AIzaSyCuj8heRtWx_X1Vd9cR_ciLphCxEkZeCwE&callback=initMap&libraries=&v=weekly\" ";
         echo "async";
-        echo "></script>\"";
+        echo "></script>\"\n";
 
-        echo "<div class=\"container-fluid\" id=\"contact-popup\">";
-            echo "<div class=\"row d-flex justify-content-center align-items-center\">";
-                echo "<div class=\"col-11 col-md-3 box\">";
-                    echo "<h2 id=\"place\"></h2>";
-                    echo "<p id=\"risk\"></p>";
-                    echo "<div class=\"separator\"></div>";
-                    echo "<p id=\"address\"></p>";
-                    echo "<p id=\"suburb\"></p>";
-                    echo "<p id=\"date\"></p>";
-                    echo "<p id=\"arrival-time\"></p>";
-                    echo "<p id=\"departure-time\"></p>";
-                    echo "<p id=\"close-popup\"><i class=\"fas fa-times\"></i></p>";
-                echo "</div>";
-            echo "</div>";
-        echo "</div>";
+        echo "<div class=\"container-fluid\" id=\"contact-popup\">\n";
+            echo "<div class=\"row d-flex justify-content-center align-items-center\">\n";
+                echo "<div class=\"col-11 col-md-3 box\">\n";
+                    echo "<h2 id=\"place\"></h2>\n";
+                    echo "<p id=\"risk\"></p>\n";
+                    echo "<div class=\"separator\"></div>\n";
+                    echo "<p id=\"address\"></p>\n";
+                    echo "<p id=\"suburb\"></p>\n";
+                    echo "<p id=\"date\"></p>\n";
+                    echo "<p id=\"arrival-time\"></p>\n";
+                    echo "<p id=\"departure-time\"></p>\n";
+                    echo "<p id=\"close-popup\"><i class=\"fas fa-times\"></i></p>\n";
+                echo "</div>\n";
+            echo "</div>\n";
+        echo "</div>\n";
     }
 
 }
--- view/_head.php
+++ view/_head.php
@@ -5,20 +5,18 @@ class Head {
     function init($args) {
         // $args = [1] Page title
 
-        echo "<meta charset=\"UTF-8\">";
-        echo "<meta http-equiv=\"X-UA-Compatible\" content=\"IE=edge\">";
-        echo "<meta name=\"viewport\" content=\"width=device-width, initial-scale=1.0\">";
-        echo "<meta charset=\"utf-8\">";
-        echo "<meta name=\"viewport\" content=\"width=device-width, initial-scale=1\">";
-        echo "<meta name=\"description\" content=\"COVID-19 contact tracing map for Queensland, Australia.\">";
-        echo "<meta name=\"author\" content=\"Yohanes Mario\">";
-        echo "<link rel=\"icon\" href=\"images/favicon.png\">";
-        echo "<script src=\"https://kit.fontawesome.com/199e4cee19.js\"></script>";
-        echo "<link href=\"https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/css/bootstrap.min.css\" rel=\"stylesheet\" integrity=\"sha384-EVSTQN3/azprG1Anm3QDgpJLIm9Nao0Yz1ztcQTwFspd3yD65VohhpuuCOmLASjC\" crossorigin=\"anonymous\">";
-        echo "<link href=\"css/styles.css\" rel=\"stylesheet\">";
-        echo "<script src=\"https://unpkg.com/@googlemaps/markerclustererplus/dist/index.min.js\"></script>";
-        echo "<script src=\"js/scripts.js\"></script>";
-        echo "<title>$args[1]</title>";
+        echo "\n<meta charset=\"UTF-8\">\n";
+        echo "<meta http-equiv=\"X-UA-Compatible\" content=\"IE=edge\">\n";
+        echo "<meta name=\"viewport\" content=\"width=device-width, initial-scale=1.0\">\n";
+        echo "<meta name=\"description\" content=\"COVID-19 contact tracing map for Queensland, Australia.\">\n";
+        echo "<meta name=\"author\" content=\"Yohanes Mario\">\n";
+        echo "<link rel=\"icon\" href=\"images/favicon.png\">\n";
+        echo "<script src=\"https://kit.fontawesome.com/199e4cee19.js\"></script>\n";
+        echo "<link href=\"https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/css/bootstrap.min.css\" rel=\"stylesheet\" integrity=\"sha384-EVSTQN3/azprG1Anm3QDgpJLIm9Nao0Yz1ztcQTwFspd3yD65VohhpuuCOmLASjC\" crossorigin=\"anonymous\">\n";
+        echo "<link href=\"css/styles.css\" rel=\"stylesheet\">\n";
+        echo "<script src=\"https://unpkg.com/@googlemaps/markerclustererplus/dist/index.min.js\"></script>\n";
+        echo "<script src=\"js/scripts.js\"></script>\n";
+        echo "<title>$args[1]</title>\n";
     }
 
 }
