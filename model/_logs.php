08b804a5e529ba29dc242875be89f908b50fabfd
sparepillowgit <heropedmeintothis@gmail.com>
2021-08-05 00:29:29 +1000

    Update _logs.php

--- model/_logs.php
+++ model/_logs.php
@@ -3,7 +3,7 @@
 class Logs {
 
     function query($pdo) {
-        $query = "SELECT date_format(time, '%d/%m/%Y %k:%i %p') as 'time' FROM logs WHERE id = (SELECT MAX(id) FROM logs)";
+        $query = "SELECT date_format(time, '%d/%m/%Y %l:%i %p') as 'time' FROM logs WHERE id = (SELECT MAX(id) FROM logs)";
         $stmt = $pdo->prepare($query);
         $stmt->execute();
 
